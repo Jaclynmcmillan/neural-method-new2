f8e418e694fb7195c0e728d7399a2d9a1d60c3bd
Jaclynmcmillan <Jaclyn.m.mcmillan@gmail.com>
2025-12-05 18:23:09 -0800

    Create Layout.tsx

---
+++ components/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-primary flex flex-col font-inter text-neutral-light selection:bg-accent selection:text-primary">
+      <Navbar />
+      <main className="flex-grow pt-20">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
